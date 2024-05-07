593df9eb1e5e684d0708e4a0cbe695186d04167f
frédéric guyon <frederic.guyon@u-paris.fr>
2024-05-07 16:15:32 +0200

    Update pepit.R

--- inst/pepit.R
+++ inst/pepit.R
@@ -23,7 +23,7 @@ set.pepit("MINCLIQUE",6)
 set.pepit("MINSCORE", 0) # tous les scores
 set.pepit("MAXCLASHES", 100) # clashes allowed
 set.pepit("NBCLIQUES", 5)
-set.pepit("NBHITS", 20)
+set.pepit("NBHITS", 1)
 set.pepit("POSE", FALSE)
 set.pepit("TYPES", c("A","C","O","N","c","o","b","n","a")) # all atoms
 #set.pepit("TYPES", c("A","C","O","N")) # backbone atoms
